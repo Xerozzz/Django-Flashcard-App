45913f623e85ba601295df018c076fdecf07683e
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2022-11-22 21:27:42 +0800

    Revert "test"
    This reverts commit 0f4c36412911eb65e060a4a6820c99dd6496115f.

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vx)&)h1hdg8w)9az@1u#o!p@7fyg!#2gm$=v0=&m6k9b)#ut5k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -125,5 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-django_heroku.settings(locals())
